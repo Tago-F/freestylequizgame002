5c4047e2fd068ee764e493b8c2fd415a786cfeea
Tago-F <snake112381@gmail.com>
2025-11-15 22:42:39 +0900

    CORS 設定に IP も追加。

--- backend/src/main/java/com/tagoapp/backend/config/CorsConfig.java
+++ backend/src/main/java/com/tagoapp/backend/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/**") // Adjust this to your API base path
-                .allowedOrigins("http://localhost:4200") // Angular development server
+                .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200") // Allow both localhost and 127.0.0.1
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
